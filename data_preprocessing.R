794229b46c3586d3e3a840d3286f587b2b5c4335
Junhee Lee <junheelee@Junhees-MacBook-Pro.local>
2024-10-03 01:40:41 -0400

    modified response variable

--- data_preprocessing.R
+++ data_preprocessing.R
@@ -2,41 +2,42 @@ library(readr)
 library(tidyverse)
 library(dplyr)
 
-user_file <- "data/yelp_csv/yelp_academic_dataset_user_reduced.csv"
-business_file <- "data/yelp_csv/yelp_academic_dataset_business.csv"
-review_file1 <- "data/yelp_csv/yelp_academic_dataset_review_reduced1.csv"
-review_file2 <- "data/yelp_csv/yelp_academic_dataset_review_reduced2.csv"
+user_file <- "data/reduced/yelp_academic_dataset_user_reduced.csv"
+business_file <- "data/reduced/yelp_academic_dataset_business_reduced.csv"
+review_file <- "data/reduced/yelp_academic_dataset_review_reduced.csv"
+
+# review_file1 <- "data/reduced/yelp_academic_dataset_review_reduced1.csv"
+# review_file2 <- "data/reduced/yelp_academic_dataset_review_reduced2.csv"
 
 # user_file <- "data/test/user_reduced_test.csv"
 # business_file <- "data/test/business_test.csv"
 # review_file <- "data/test/review_reduced_test.csv"
 
+review <- read_csv(review_file)
 user <- read_csv(user_file)
 business <- read_csv(business_file)
-review1 <- read_csv(review_file1)
-review2 <- read_csv(review_file2)
-review <- rbind(review1, review2)
-# review <- read_csv(review_file)
 
-# View(user)
-# View(business)
-# View(review)
+# review1 <- read_csv(review_file1)
+# review2 <- read_csv(review_file2)
+# review <- rbind(review1, review2)
+
+View(user)
+View(business)
+View(review)
 
 new_user <- subset(user, select = c(fans, average_stars, elite,
-                                    review_count, user_id))
+                                    review_count, user_id, compliments))
 new_user <- mutate(new_user, elite = as.integer(str_length(elite) > 3))
 
-new_business <- subset(business, select = c(business_id, review_count))
-
-new_review <- subset(review, select = c(business_id, user_id, stars, useful))
-new_review <- left_join(new_review, new_business, by = "business_id")
-# new_review <- new_review %>% rename(stars = stars.x, business_stars = stars.y)
+new_business <- subset(business, select = c(business_id))
+new_review <- subset(review, select = c(business_id, user_id, stars, useful, word_count))
+new_review <- right_join(new_review, new_business, by = "business_id")
 
 new <- new_review %>%
   group_by(user_id) %>%
   summarise(
-    average_business_review_count = mean(review_count),
-    average_useful = mean(useful)
+    average_useful = mean(useful),
+    extreme_ratings = sum(stars == 1) + sum(stars == 5)
   )
 
 data <- left_join(new, new_user, by = "user_id")
--- make_dataset.py
+++ make_dataset.py
@@ -4,24 +4,40 @@ f = "./data/yelp_academic_dataset_"
 f_business = f + "business.csv"
 f_review = f + "review.csv"
 f_user = f + "user.csv"
-f_review_reduced = "./data/yelp_csv/yelp_academic_dataset_review_reduced.csv"
+f_user_reduced = "./data/past/yelp_academic_dataset_user_reduced.csv"
+f_review_reduced = "./data/reduced/yelp_academic_dataset_review_reduced.csv"
 
 def user(f):
     df = pd.read_csv(f)
-    # dst = "./data/yelp_academic_dataset_user_reduced.csv"
-    dst = "test.csv"
+    dst = "./data/reduced/yelp_academic_dataset_user_reduced.csv"
+    # dst = "test.csv"
 
-    df["friends_count"] = df["friends"].str.split(",").size + 1
+    df["friends_count"] = df["friends"].str.count(",") + 1
     df = df.drop("friends", axis=1)
+    df["compliments"] = df["compliment_hot"] + df["compliment_more"] + df["compliment_profile"] + df["compliment_cute"] + df["compliment_list"] + \
+                        df["compliment_note"] + df["compliment_plain"] + df["compliment_cool"] + df["compliment_funny"] + df["compliment_writer"] + df["compliment_photos"]
+    df = df.drop(["compliment_hot", "compliment_more", "compliment_profile", "compliment_cute", "compliment_list", 
+                  "compliment_note", "compliment_plain", "compliment_cool", "compliment_funny", "compliment_writer", "compliment_photos"], axis=1)
 
     df.to_csv(dst, index=False)
 
 def review(f):
     df = pd.read_csv(f)
-    dst = "./data/yelp_academic_dataset_review_reduced.csv"
+    # dst = "test.csv"
+    dst = "./data/reduced/yelp_academic_dataset_review_reduced.csv"
 
-    df["word_count"] = df["text"].str.len()
+    df["word_count"] = df["text"].str.count(" ") + 1
     df = df.drop("text", axis=1)
+    
+    df.to_csv(dst, index=False)
+
+def business(f):
+    df = pd.read_csv(f, low_memory=False)
+    dst = "./data/reduced/yelp_academic_dataset_business_reduced.csv"
+    
+    df = df[["business_id", "stars", "review_count", "state"]]
+    df = df[df['state'].str.contains("AB")]
+    df = df.drop("state", axis=1)
 
     df.to_csv(dst, index=False)
 
@@ -37,8 +53,10 @@ def split_df(f):
     df1.to_csv(dst1, index=False)
     df2.to_csv(dst2, index=False)
 
-# user(f_user)
-# review(f_review)
+if __name__ == "__main__":
+    # user(f_user)
+    # review(f_review)
+    # split_df(f_review_reduced)
 
-# split_df(f_review_reduced)
-
+    # review("./data/test/review_test.csv")
+    business(f_business)
